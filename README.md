0d8b6bd9c56141baac4814500ad83913d3cf2741
Piotr Pospiech <ptkpospiech@gmail.com>
2020-06-01 21:46:41 +0200

    added 3d box

--- README.md
+++ README.md
@@ -12,10 +12,6 @@ https://gluonhq.com/products/javafx/
 
 https://github.com/FXMisc/RichTextFX/releases/download/v0.10.5/richtextfx-fat-0.10.5.jar
 
-##### Pierdolnik do kształtów 3D:
-
-https://repo1.maven.org/maven2/org/fxyz3d/fxyz3d/0.5.2/fxyz3d-0.5.2.jar
-
 ##### Wszystkie biblioteki należy wrzucić do folderu libs
 
 ##
@@ -25,7 +21,7 @@ https://repo1.maven.org/maven2/org/fxyz3d/fxyz3d/0.5.2/fxyz3d-0.5.2.jar
 * File -> Reload All from Disk && Invalidate Caches / Restart...
 * File -> Project Structure
     * Project (SDK, Project compiler output - ustaw na folder out)
-    * Modules ("RichTextFX" oraz "Pierdolnik do kształtów 3D")
+    * Modules (RichTextFX)
     * Libraries (Punkt "Add the JavaFX library﻿" - https://www.jetbrains.com/help/idea/javafx.html?fbclid=IwAR0bH4XfeMjbF5JQRrvYriKi284PiOGw3hVEyBQmKbwEEm1ciS2eE3GIaIs#add-javafx-lib)
 * Folder src -> Mark Directory as -> Sources Root
 * Run Main
--- src/zpi/Main.java
+++ src/zpi/Main.java
@@ -2,15 +2,25 @@ package zpi;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 
 public class Main extends Application {
+
+    private double mousePosX, mousePosY;
+    private double mouseOldX, mouseOldY;
+    private final Rotate rotateX = new Rotate(-1, Rotate.X_AXIS);
+    private final Rotate rotateY = new Rotate(-1, Rotate.Y_AXIS);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -26,7 +36,39 @@ public class Main extends Application {
         primaryStage.show();
 
         addCodeArea(scene);
-        addVisualisationArea(scene);
+
+        // 3D BOX
+        Box box = new Box(5, 5, 5);
+        box.setMaterial(new PhongMaterial(Color.GREENYELLOW));
+
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.getTransforms().addAll(rotateX, rotateY, new Translate(0, 0, -20));
+
+        Group root3D = new Group(camera,box);
+        SubScene subScene = new SubScene(root3D, 300, 300, true, SceneAntialiasing.BALANCED);
+        subScene.setFill(Color.AQUAMARINE);
+        subScene.setCamera(camera);
+
+        StackPane stackPane = (StackPane) scene.lookup("#3dStackPane");
+
+        subScene.widthProperty().bind(stackPane.widthProperty());
+        subScene.heightProperty().bind(stackPane.heightProperty());
+
+        stackPane.getChildren().add(subScene);
+
+        subScene.setOnMousePressed((MouseEvent me) -> {
+            mouseOldX = me.getSceneX();
+            mouseOldY = me.getSceneY();
+        });
+
+        subScene.setOnMouseDragged((MouseEvent me) -> {
+            mousePosX = me.getSceneX();
+            mousePosY = me.getSceneY();
+            rotateX.setAngle(rotateX.getAngle()-(mousePosY - mouseOldY));
+            rotateY.setAngle(rotateY.getAngle()+(mousePosX - mouseOldX));
+            mouseOldX = mousePosX;
+            mouseOldY = mousePosY;
+        });
     }
 
     private void addCodeArea(Scene scene) {
@@ -41,20 +83,6 @@ public class Main extends Application {
         codeArea.prefWidthProperty().bind(codeAreaAnchorPane.widthProperty());
         codeArea.prefHeightProperty().bind(codeAreaAnchorPane.heightProperty());
     }
-
-    private void addVisualisationArea(Scene scene){
-        Group group = new Group();
-        Model3DView viewArea = new Model3DView(scene, group);
-        viewArea.createScene();
-        //viewArea.handleMouse(scene, group);
-
-        AnchorPane visualisationAnchorPane = (AnchorPane) scene.lookup("#visualisationAreaAnchorPane");
-        visualisationAnchorPane.getChildren().add(group);
-
-        group.translateXProperty().bind(visualisationAnchorPane.widthProperty().divide(2));
-        group.translateYProperty().bind(visualisationAnchorPane.heightProperty().divide(2));
-    }
-
 
     public static void main(String[] args) {
         launch(args);
