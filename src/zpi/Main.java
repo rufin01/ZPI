9b56a27b132b50ce54d82f1d20dc328549bf2540
Piotr Pospiech <ptkpospiech@gmail.com>
2020-06-02 14:55:31 +0200

    added XForm and molecule code

--- src/zpi/Main.java
+++ src/zpi/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
@@ -19,13 +21,12 @@ public class Main extends Application {
 
     private double mousePosX, mousePosY;
     private double mouseOldX, mouseOldY;
-    private final Rotate rotateX = new Rotate(-1, Rotate.X_AXIS);
-    private final Rotate rotateY = new Rotate(-1, Rotate.Y_AXIS);
+    private final Rotate rotateX = new Rotate(-20, Rotate.X_AXIS);
+    private final Rotate rotateY = new Rotate(-20, Rotate.Y_AXIS);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        primaryStage.setTitle("zpi");
 
         Scene scene = new Scene(root, 1024, 768);
         primaryStage.setScene(scene);
@@ -33,6 +34,7 @@ public class Main extends Application {
         primaryStage.setMinWidth(1000);
         primaryStage.setMinHeight(800);
 
+        primaryStage.setTitle("zpi");
         primaryStage.show();
 
         addCodeArea(scene);
@@ -41,15 +43,88 @@ public class Main extends Application {
         Box box = new Box(5, 5, 5);
         box.setMaterial(new PhongMaterial(Color.GREENYELLOW));
 
-        PerspectiveCamera camera = new PerspectiveCamera(true);
-        camera.getTransforms().addAll(rotateX, rotateY, new Translate(0, 0, -20));
+        // ------------
 
-        Group root3D = new Group(camera,box);
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial whiteMaterial = new PhongMaterial();
+        whiteMaterial.setDiffuseColor(Color.WHITE);
+        whiteMaterial.setSpecularColor(Color.LIGHTBLUE);
+
+        final PhongMaterial greyMaterial = new PhongMaterial();
+        greyMaterial.setDiffuseColor(Color.DARKGREY);
+        greyMaterial.setSpecularColor(Color.GREY);
+
+        // Molecule Hierarchy
+        // [*] moleculeXform
+        //     [*] oxygenXform
+        //         [*] oxygenSphere
+        //     [*] hydrogen1SideXform
+        //         [*] hydrogen1Xform
+        //             [*] hydrogen1Sphere
+        //         [*] bond1Cylinder
+        //     [*] hydrogen2SideXform
+        //         [*] hydrogen2Xform
+        //             [*] hydrogen2Sphere
+        //         [*] bond2Cylinder
+        XForm moleculeXform = new XForm();
+        XForm oxygenXform = new XForm();
+        XForm hydrogen1SideXform = new XForm();
+        XForm hydrogen1Xform = new XForm();
+        XForm hydrogen2SideXform = new XForm();
+        XForm hydrogen2Xform = new XForm();
+
+        Sphere oxygenSphere = new Sphere(40.0);
+        oxygenSphere.setMaterial(redMaterial);
+
+        Sphere hydrogen1Sphere = new Sphere(30.0);
+        hydrogen1Sphere.setMaterial(whiteMaterial);
+        hydrogen1Sphere.setTranslateX(0.0);
+
+        Sphere hydrogen2Sphere = new Sphere(30.0);
+        hydrogen2Sphere.setMaterial(whiteMaterial);
+        hydrogen2Sphere.setTranslateZ(0.0);
+
+        Cylinder bond1Cylinder = new Cylinder(5, 100);
+        bond1Cylinder.setMaterial(greyMaterial);
+        bond1Cylinder.setTranslateX(50.0);
+        bond1Cylinder.setRotationAxis(Rotate.Z_AXIS);
+        bond1Cylinder.setRotate(90.0);
+
+        Cylinder bond2Cylinder = new Cylinder(5, 100);
+        bond2Cylinder.setMaterial(greyMaterial);
+        bond2Cylinder.setTranslateX(50.0);
+        bond2Cylinder.setRotationAxis(Rotate.Z_AXIS);
+        bond2Cylinder.setRotate(90.0);
+
+        moleculeXform.getChildren().add(oxygenXform);
+        moleculeXform.getChildren().add(hydrogen1SideXform);
+        moleculeXform.getChildren().add(hydrogen2SideXform);
+        oxygenXform.getChildren().add(oxygenSphere);
+        hydrogen1SideXform.getChildren().add(hydrogen1Xform);
+        hydrogen2SideXform.getChildren().add(hydrogen2Xform);
+        hydrogen1Xform.getChildren().add(hydrogen1Sphere);
+        hydrogen2Xform.getChildren().add(hydrogen2Sphere);
+        hydrogen1SideXform.getChildren().add(bond1Cylinder);
+        hydrogen2SideXform.getChildren().add(bond2Cylinder);
+
+        hydrogen1Xform.setTx(100.0);
+        hydrogen2Xform.setTx(100.0);
+        hydrogen2SideXform.setRotateY(0);
+
+        // ------------
+
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.getTransforms().addAll(rotateX, rotateY, new Translate(0, 0, -35));
+
+        Group root3D = new Group(camera, box);
         SubScene subScene = new SubScene(root3D, 300, 300, true, SceneAntialiasing.BALANCED);
         subScene.setFill(Color.AQUAMARINE);
         subScene.setCamera(camera);
 
-        StackPane stackPane = (StackPane) scene.lookup("#3dStackPane");
+        StackPane stackPane = (StackPane) scene.lookup("#stackPane");
 
         subScene.widthProperty().bind(stackPane.widthProperty());
         subScene.heightProperty().bind(stackPane.heightProperty());
